587f970de7e8e0b56b11a7d76c5dc19e1b51bbd6
Sam <samwelnyandoro01@gmail.com>
2025-01-25 14:07:41 +0300

    Refactor: Simplify Permissions and UI Handling
    - Removes redundant code for setting translucent status and light status bar on various API levels.
    - Simplifies permission checks and requests, removing unnecessary comments.
    - Removes `setWindowFlag` and related logic, `WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS`.

--- app/src/main/java/com/weatherapp/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/weatherapp/weatherapp/ui/MainActivity.kt
@@ -68,27 +68,9 @@ class MainActivity : AppCompatActivity(), LocationListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         toolbarBinding = ToolbarBinding.bind(findViewById(R.id.toolbarLayout))
-        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
-        }
-        if (Build.VERSION.SDK_INT >= 19) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                window.decorView.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            }
-        }
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
-            window.statusBarColor = Color.TRANSPARENT
-        }
-
-        // Set Permissions
         val myVersion = Build.VERSION.SDK_INT
         if (myVersion > Build.VERSION_CODES.LOLLIPOP_MR1) {
             if (checkIfAlreadyhavePermission() && checkIfAlreadyhavePermission2()) {
-                // Permissions already granted
             } else {
                 requestPermissions(permissionArrays, 101)
             }
